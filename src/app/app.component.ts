9e66b8df6f82c02da7eb2bc8dfd056be0e50442e
진호성 <yuuki@newlayer.kr>
2023-04-05 15:03:30 +0900

    fullcalendar 적용

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { Calendar } from '@fullcalendar/core';
+
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit() :void {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('calendar') calendarComponent!: FullCalendarComponent;
+
+  ngAfterViewInit() {
+    let calendarEl: HTMLElement = document.getElementById('calendar')!;
+    let calendar = new Calendar(calendarEl, {
+      plugins: [ dayGridPlugin ]
+      // options here
+    });
+  
+    calendar.render();
+
   }
 }
